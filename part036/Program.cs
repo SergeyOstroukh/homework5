f6f33d27bf5489ed6b21c0aba634b15799362a9b
Sergey <hardmass3ffect@gmail.com>
2023-01-30 10:20:08 +0300

    сделал homework 5

---
+++ part036/Program.cs
@@ -0,0 +1,47 @@
+﻿// // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int SumElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)         // 0 1 2 3 4 5 6 7 8 9
+    {                                              // 1 2 3 4 5 6 7 8 9 10
+        if (i % 2 == 1)
+            sum = sum + array[i];
+    }
+    return sum;
+}
+
+int[] arr = CreateArray(10, -5, 5);
+PrintArray(arr);
+int sumElements = SumElements(arr);
+Console.WriteLine($" -> {SumElements(arr)}");
---
+++ part038/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементами массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+double[] CreateArrayRndDouble(int size, double min, double max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        array[i] = Math.Round(num, 1);
+    }
+    return array;
+}
+
+void PrintArrayDouble(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+
+    }
+    Console.Write("]");
+}
+
+double MaxDigit(double[] array)
+{
+    int i = 0;
+    double max = array[i];
+    while (i < array.Length)
+    {
+        if (array[i] > max) max = array[i];
+        i++;
+
+    }
+    return max;
+}
+double MinDigit(double[] array)
+{
+    int i = 0;
+    double min = array[i];
+    while (i < array.Length)
+    {
+        if (array[i] < min) min = array[i];
+        i++;
+
+    }
+    return min;
+}
+
+double DifferenceMinMax(double MaxDigit, double MinDigit)
+{
+    double result = Math.Round(MaxDigit - MinDigit, 2);
+    return result;
+}
+
+
+double[] arr = CreateArrayRndDouble(10, 0, 10);
+PrintArrayDouble(arr);
+double maximum = MaxDigit(arr);
+double minimum = MinDigit(arr);
+double diff = DifferenceMinMax(maximum, minimum);
+Console.WriteLine($" -> {diff}");
+
